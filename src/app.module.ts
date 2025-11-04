4900c5ee16741080dd503db137c956dbc4592386
Toushik-Ahmed <Pronay5271@gmail.com>
2025-11-04 23:27:52 +0600

    added databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetModule } from './tweet/tweet.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TweetModule, UsersModule],
+  imports: [
+    TweetModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        // synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '1998',
+        database: 'nestjs',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
